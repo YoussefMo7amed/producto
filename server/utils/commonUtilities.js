9d9b6771b8b276ac154c596149c6c14f862ed80d
youssefmo7amed <youssef.mo7amed@gmail.com>
2024-01-11 18:50:42 +0200

    fix commonUtilities.js (checkForDuplicates) function - check for all differenct datatypes by casting them into string

--- server/utils/commonUtilities.js
+++ server/utils/commonUtilities.js
@@ -1,6 +1,7 @@
 // duplicationChecker.js
 const checkForDuplicates = (array) => {
-    const uniqueItems = new Set(array);
+    const stringArray = array.map((item) => String(item));
+    const uniqueItems = new Set(stringArray);
     return uniqueItems.size !== array.length;
 };
 
