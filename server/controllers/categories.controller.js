b981bc7cbca78bc7bd1cf9c5f96bbf4df4a729b5
youssefmo7amed <youssef.mo7amed@gmail.com>
2024-01-11 18:52:41 +0200

    fix categories controller - handle updating productIds arrary

--- server/controllers/categories.controller.js
+++ server/controllers/categories.controller.js
@@ -7,12 +7,14 @@ const Product = require("../models/Product.model");
 // GET
 exports.getAllCategories = async (req, res) => {
     try {
-        // Fetch all categories from the database
         const categories = await Category.find();
         res.json(categories);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+
+        let message = error.message || "Internal Server Error";
+
+        res.status(500).json({ message });
     }
 };
 
@@ -20,7 +22,6 @@ exports.getCategoryById = async (req, res) => {
     const categoryId = req.params.id;
 
     try {
-        // Fetch a specific category by ID from the database
         const category = await Category.findById(categoryId);
 
         if (!category) {
@@ -30,22 +31,21 @@ exports.getCategoryById = async (req, res) => {
         res.json(category);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+
+        let message = error.message || "Internal Server Error";
+
+        res.status(500).json({ message });
     }
 };
 
 async function checkProductIdValidation(productIds) {
-    // Use map to create an array of promises
     const promises = productIds.map(async (productId) => {
         let found = await Product.findById(productId);
-        console.log(found + "\\n");
-        return found === null; // Return true if not found, false otherwise
+        return found === null;
     });
 
-    // Wait for all promises to resolve
     const results = await Promise.all(promises);
 
-    // Check if any productId was not found
     return results.some((result) => result);
 }
 
@@ -53,7 +53,6 @@ async function checkProductIdValidation(productIds) {
 exports.createCategory = async (req, res) => {
     const { name, description, productIds } = req.body;
 
-    // Check for duplication in productIds
     if (checkForDuplicates(productIds)) {
         return res.status(400).json({ error: "Duplicate productIds found" });
     }
@@ -63,7 +62,6 @@ exports.createCategory = async (req, res) => {
     }
 
     try {
-        // Create a new category in the database
         const newCategory = new Category({
             name,
             description,
@@ -77,19 +75,44 @@ exports.createCategory = async (req, res) => {
         res.status(201).json(savedCategory);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+
+        let message = error.message || "Internal Server Error";
+
+        res.status(500).json({ message });
     }
 };
+
 // PUT
 exports.updateCategory = async (req, res) => {
     const categoryId = req.params.id;
-    const { name, description } = req.body;
+    const { name, description, productIds } = req.body;
 
     try {
-        // Update a specific category by ID in the database
+        const existingCategory = await Category.findById(categoryId);
+
+        if (!existingCategory) {
+            return res.status(404).json({ message: "Category not found" });
+        }
+
+        const newProductIds = productIds
+            ? productIds.map(
+                  (productId) => new mongoose.Types.ObjectId(productId)
+              )
+            : [];
+
+        const updatedProductIds = [
+            ...new Set([...existingCategory.productIds, ...newProductIds]),
+        ];
+
+        if (checkForDuplicates(updatedProductIds)) {
+            return res
+                .status(400)
+                .json({ error: "Duplicate productIds found" });
+        }
+
         const updatedCategory = await Category.findByIdAndUpdate(
             categoryId,
-            { name, description },
+            { name, description, productIds: updatedProductIds },
             { new: true }
         );
 
@@ -100,7 +123,10 @@ exports.updateCategory = async (req, res) => {
         res.json(updatedCategory);
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+
+        let message = error.message || "Internal Server Error";
+
+        res.status(500).json({ message });
     }
 };
 
@@ -109,7 +135,6 @@ exports.deleteCategory = async (req, res) => {
     const categoryId = req.params.id;
 
     try {
-        // Delete a specific category by ID from the database
         const deletedCategory = await Category.findByIdAndDelete(categoryId);
 
         if (!deletedCategory) {
@@ -119,6 +144,9 @@ exports.deleteCategory = async (req, res) => {
         res.json({ message: "Category deleted successfully" });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "Internal Server Error" });
+
+        let message = error.message || "Internal Server Error";
+
+        res.status(500).json({ message });
     }
 };
