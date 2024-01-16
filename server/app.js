26dd45aaffd8a9dad0bd03136004af85873161db
youssefmo7amed <youssef.mo7amed@gmail.com>
2024-01-16 16:14:26 +0200

    Implement CORS in server/app.js and specify allowed origins
    - Update server/app.js to include CORS middleware
    - Add server/allowed-origins.json to specify allowed origins for CORS

--- server/app.js
+++ server/app.js
@@ -2,11 +2,33 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 require("dotenv").config();
+const cors = require("cors");
+const fs = require("fs");
+const path = require("path");
 
 // Configuration variables
 const DB_URI = process.env.DB_URI;
 const DB_OPTIONS = JSON.parse(process.env.DB_OPTIONS);
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const Host = process.env.HOST;
+const serverHost = `${Host}:${PORT}`;
+
+// Read allowed origins from the JSON file
+const allowedOriginsPath = path.join(__dirname, "allowed-origins.json");
+const allowedOrigins = JSON.parse(fs.readFileSync(allowedOriginsPath, "utf8"));
+allowedOrigins.push(serverHost);
+console.log(allowedOrigins);
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) {
+            callback(null, true);
+        } else {
+            allowedOrigins.push(origin);
+            callback(null, true);
+        }
+    },
+};
 
 // Connect to MongoDB
 mongoose
@@ -20,6 +42,7 @@ const CURRENT_API_VERSION = "v1";
 const app = express();
 
 // Use Middlewares
+app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
 // Import routes
@@ -31,6 +54,10 @@ const reviewsRoutes = require("./routes/reviews.routes");
 app.use(`/api/${CURRENT_API_VERSION}/`, categoriesRoutes);
 app.use(`/api/${CURRENT_API_VERSION}/`, productsRoutes);
 app.use(`/api/${CURRENT_API_VERSION}/`, reviewsRoutes);
+
+//
+const uploadsRoutes = require("./routes/uploads.routes");
+app.use("/server/uploads", uploadsRoutes);
 
 // Start the server
 app.listen(PORT, () => {
